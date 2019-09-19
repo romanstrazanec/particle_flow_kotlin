3713c34cb44dfd425354fb620f0ef117ecf59065
romanstrazanec <roman.strazanec007@gmail.com>
2019-09-19 20:27:23 +0200

    attract by distance to particle

--- app/src/main/java/sk/romanstrazanec/particleflowkotlin/AttractionPoint.kt
+++ app/src/main/java/sk/romanstrazanec/particleflowkotlin/AttractionPoint.kt
@@ -1,13 +1,22 @@
 package sk.romanstrazanec.particleflowkotlin
 
+import kotlin.math.sqrt
+
 class AttractionPoint(var x: Float, var y: Float) {
     fun moveTo(x: Float, y: Float) {
         this.x = x
         this.y = y
     }
 
-    fun attract(p: Particle) {
-        p.x += x.compareTo(p.x)
-        p.y += y.compareTo(p.y)
+    private fun distance(p: Particle): Float {
+        val dx = p.x - x
+        val dy = p.y - y
+        return sqrt(dx * dx + dy * dy)
+    }
+
+    fun attract(p: Particle, f: Float = 250F) {
+        val d = distance(p)
+        p.x += f * x.compareTo(p.x) / d
+        p.y += f * y.compareTo(p.y) / d
     }
 }
